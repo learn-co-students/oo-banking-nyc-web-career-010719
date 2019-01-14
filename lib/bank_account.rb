9efb9f811c59b741d10857e9ed0d8f418be0004c
Your Name <github email address>
2019-01-14 12:41:21 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
-end
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+
+    @@all << self
+  end
+
+  #BankAccount #deposit can deposit money into its account
+  def deposit(money)
+    self.balance += money
+  end
+
+  #BankAccount #display_balance can display its balance
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  #BankAccount #valid? is valid with an open status and a balance greater than 0
+  def valid?
+    self.status == "open" && self.balance > 0 ? true : false
+  end
+
+  #BankAccount can close its account
+  def close_account
+    self.status = "closed"
+  end
+end #end of my BankAccount class
